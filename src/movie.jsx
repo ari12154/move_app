2806b4ad0506d0e02e1cce882ce06c1f9a93bde7
ari12154 <ari5780725@gmail.com>
2023-05-03 12:54:47 +0300

    new

--- src/movie.jsx
+++ src/movie.jsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MainMovie from "./comps/home/MainMovie";
 import Carusela from "./comps/home/Carusela";
-<<<<<<< HEAD
-=======
 import Test from "./comps/home/Test";
->>>>>>> 015b1c7a25d44496e59497b7c752128c1281083f
 
 export default function Movie() {
   let [data, setData] = useState();
@@ -22,10 +19,6 @@ useEffect(()=>{
   return <div>
     <MainMovie data={data} />
     <Carusela data={data} />
-<<<<<<< HEAD
-=======
-    <Test data={data}/>
->>>>>>> 015b1c7a25d44496e59497b7c752128c1281083f
     </div>;
   
 }
